390f5b7b6efc7bd3dc13ae5051bda12bfb492156
Walter Wu <walter.wu@softwire.com>
2019-07-01 11:47:16 +0100

    Move bishop

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,11 +1,13 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
 
 export default class Bishop extends Piece {
     constructor(player) {
         super(player);
+        this.moves = [];
+        for (let i = 1; i < GameSettings.BOARD_SIZE; i++) {
+            this.moves.push([i, i], [-i, -i], [i, -i], [-i, i]);
+        }
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
-    }
 }
