1547a3b463c1d4800c8a4c85d4e97c89c76c4591
Walter Wu <walter.wu@softwire.com>
2019-07-01 14:51:20 +0100

    Move queen

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,12 +1,18 @@
 import Piece from './piece';
 import GameSettings from "../gameSettings";
+import Move from "../move";
+import MoveType from "../moveType";
 
 export default class Queen extends Piece {
     constructor(player) {
         super(player);
         this.moves = [];
         for (let i = 1; i < GameSettings.BOARD_SIZE; i++) {
-            this.moves.push([i, i], [-i, -i], [i, -i], [-i, i], [i, 0], [-i, 0], [0, i], [0, -i]);
+            this.moves = this.moves.concat(Move.constructSameType([[i, i], [-i, -i], [i, -i], [-i, i]],
+                MoveType.DIAGONAL, false));
+            this.moves = this.moves.concat(Move.constructSameType([[i, 0], [-i, 0], [0, i], [0, -i]],
+                MoveType.LATERAL, false));
+
         }
     }
 
