d436ddb103b1fcbfef7a11c36bebedd61e3aaf8b
Walter Wu <walter.wu@softwire.com>
2019-07-01 11:52:30 +0100

    Move knight

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -3,9 +3,8 @@ import Piece from './piece';
 export default class Knight extends Piece {
     constructor(player) {
         super(player);
+        this.moves = [[2, -1], [2, 1], [1, 2], [1, -2], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
+
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
-    }
 }
