74e6acb3d485d5bf7509f16e9d3899af61a4080e
Walter Wu <walter.wu@softwire.com>
2019-07-01 11:44:34 +0100

    Move rook

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,11 +1,13 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     constructor(player) {
         super(player);
+        this.moves = [];
+        for (let i = 1; i < GameSettings.BOARD_SIZE; i++) {
+            this.moves.push([i, 0], [-i, 0], [0, i], [0, -i]);
+        }
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
-    }
 }
